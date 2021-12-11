cc66c4d29ae560f4e7bd9002fb34a1a99129ccd7
Sean McGinty <31000120+s3ansh33p@users.noreply.github.com>
2021-12-11 17:46:24 +0800

    Update event_handler.hpp

--- lib/core/event_handler.hpp
+++ lib/core/event_handler.hpp
@@ -1,45 +1,64 @@
-// Event Handler written by s3ansh33p - WIP
-
-/*
-
-addListener(&myFunctionName, KEY_EXP);  // add a listener to the Power Key that calls myFunctionName when pressed
-
-bool game_running = true;               // starts a game loop
-while (game_running) {                  // the game loop
-    checkEvents();                      // check for events, including key presses
-}
-
-*/
+/**
+ * @file event_handler.hpp
+ * @author Sean McGinty (newfolderlocation@gmail.com)
+ * @brief Event handler class to keep track of events and their handlers.
+ * @version 1.0
+ * @date 2021-12-11
+ * 
+ * It can be useful to track memory usage, framerate and other information.
+ * 
+ * @code{cpp}
+ * // add a listener to the Power Key that calls myFunctionName when pressed
+ * addListener(KEY_EXP, myFunctionName);  
+ * 
+ * // starts a game loop
+ * bool game_running = true;
+ * while (game_running) {
+ *     // check for events, including key presses
+ *     checkEvents();                      
+ * }
+ * @endcode
+ */
 
 #pragma once
-#include "../../../calc.hpp"
+#include "../../calc.hpp"
 
-uint32_t key1, key2;
-typedef void (*printer_t)(int);
+uint32_t ev_key1, ev_key2;
+uint32_t ev_old1 = 0;
+uint32_t ev_old2 = 0;
 
-// Class listerner that stores a function pointer
-class Listener {
-    public:
-        Keys1 key;
-        printer_t p;
-};
+// Currently only storing 5 events
+Keys1 listeners[5] = {KEY_CLEAR, KEY_CLEAR, KEY_CLEAR, KEY_CLEAR, KEY_CLEAR};
+void (*func_ptr[5])() = {0, 0, 0, 0, 0};
+uint8_t listener_count = 0;
 
-// Listeners array with size 10
-Listener listeners[10];
-
-// Will also include touch events in the future
-void checkEvents() {
-    getKey(&key1, &key2);
-    for (int i = 0; i < sizeof(listeners); i++) {
-        // if the key is pressed, call the listener
-        if (testKey(key1, key2, listeners[i].key)) {
-            (*listeners[i].p);
-        }
-    }
+void addListener(Keys1 key, void (*func)()) {
+   listeners[listener_count] = key;
+   func_ptr[listener_count] = func;
+   listener_count++;
 }
 
-// function that creates a listener and adds it to the listeners array
-void addListener(printer_t p, Keys1 key) {
-    listeners[sizeof(listeners)+1].key = key;
-    listeners[sizeof(listeners)+1].p = p;
+void checkEvents() {
+   //Read the keyboard
+   ev_old1 = ev_key1;
+   ev_old2 = ev_key2;
+   getKey(&ev_key1, &ev_key2);
+
+   if (ev_old1 != ev_key1 || ev_old2 != ev_key2) {
+      for (uint8_t i = 0; i < listener_count; i++) {
+         // Check if the listener is not 0
+         if (listeners[i] != KEY_CLEAR) {
+            // Check if the key is pressed
+            if (testKey(ev_key1,ev_key2,listeners[i])) {
+               // check for the function pointer
+               if (func_ptr[i] != 0) {
+                  // call the function
+                  (*func_ptr[i])();
+               }
+            }
+         } else {
+            break; // no need to check the rest of the listeners
+         }
+      }
+   }
 }
