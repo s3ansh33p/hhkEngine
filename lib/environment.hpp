f39b85dc9db177e998f5d17c4c6c2758c4753ec9
Sean McGinty <31000120+s3ansh33p@users.noreply.github.com>
2021-12-12 18:56:42 +0800

    Create environment.hpp

---
+++ lib/environment.hpp
@@ -0,0 +1,10 @@
+/**
+ * @file environment.hpp
+ * @author Sean McGinty (newfolderlocation@gmail.com)
+ * @brief Engine settings
+ * @version 1.0
+ * @date 2021-12-12
+ */
+
+int GRAVITY = -10; // gravity that is applied to all objects
+int DebugYCounter = 5; // used to render debug text
